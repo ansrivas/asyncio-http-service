74323adf23123b90601caab07ef3414bd13f9a94
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-10-08 17:41:44 +0200

    name updaates

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='date_manager',
-    version='0.2.0',
+    name='asyncio_http_service',
+    version='0.1.0',
     include_package_data=True,
     install_requires=['ruamel.yaml==0.15.34', 'SQLAlchemy==1.1.14',
                       'asyncpg==0.12.0', 'voluptuous==0.10.5',
