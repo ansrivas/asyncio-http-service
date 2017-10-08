fae02bc4b9b67757cde851bd8e0b61f6f93fb9dd
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-10-08 23:01:04 +0200

    cleanup comments

--- app/main.py
+++ app/main.py
@@ -4,9 +4,11 @@
 
 import uvloop
 import asyncio
+import jinja2
+import aiohttp_jinja2
 from aiohttp import web
-from app.db import init_db, close_db
 from app.routes.index import Index
+from app.db import init_db, close_db
 from app.utils import read_config_from_env
 
 
@@ -20,29 +22,19 @@ def setup_routes(app):
 
 def create_app():
     """Configure and create the aiohttp app."""
-    # Create a uvloop
     loop = uvloop.new_event_loop()
-
-    # Assing it to asyncio
     asyncio.set_event_loop(loop)
 
     app = web.Application()
     app['config'] = read_config_from_env('config_file')
-    # app['pool'] = asyncio.get_event_loop().run_until_complete(init_db(config=app['config']))
-
-    # aiohttp_jinja2.setup(
-    #     app, loader=jinja2.PackageLoader('datemanager', 'templates'))
-
-    # another way to create connection to the database
     app.on_startup.append(init_db)
-
-    # shutdown db connection on exit
     app.on_cleanup.append(close_db)
 
-    # setup views and routes
+    aiohttp_jinja2.setup(
+        app, loader=jinja2.PackageLoader('app', 'templates'))
+
     setup_routes(app)
 
-    # setup_middlewares(app)
     return app
 
 
