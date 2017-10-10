1a5a6c269cc356451e779df083ee368ff5cb8ca4
Ankur Srivastava <ankur.srivastava@recogizer.de>
2017-10-10 19:19:09 +0200

    get_all_users() returns a list of records, so we need to be careful

--- tests/test_services.py
+++ tests/test_services.py
@@ -16,3 +16,20 @@ async def test_get_all_users(pool_with_tables):
     assert status.data is None
     assert not status.is_success
     assert status.message == "No registered users found"
+
+
+@pytest.mark.asyncio
+async def test_get_one_user(pool_with_tables):
+    """."""
+
+    async with pool_with_tables.acquire() as conn:
+        async with conn.transaction():
+            await conn.execute('''INSERT INTO users(id, created_timestamp, modified_timestamp, email_address) VALUES (1, '2016-11-12', '2016-11-13', 'myemail@gmail.com')''')
+
+    async with pool_with_tables.acquire() as conn:
+        async with conn.transaction():
+            status = await get_all_users(conn)
+
+    assert status.data[0]['email_address'] == "myemail@gmail.com"
+    assert status.is_success
+    assert status.message == ""
